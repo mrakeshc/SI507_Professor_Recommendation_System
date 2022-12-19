9890b8c27196552d333148255652aa2b5edb304c
mrakeshc <mrakeshc@umich.edu>
2022-12-18 19:31:11 -0500

    init commit

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+"""
+Importing Flask and the tree object from the main python file
+"""
 from flask import Flask, render_template
 from main import scraping_caching
+
+#Creating an instance of the Flask class
 app = Flask(__name__)
 
-
+#Telling route() decorator what url should trigger our function
 @app.route('/')
 def index():
     data = scraping_caching()
+    #Rendering the template using the tree data extracted
     return render_template('index.html', data=data)
 
-
+#As usual main function call
 if __name__ == "__main__":
     app.run(debug=True)
